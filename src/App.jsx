4c7f9e7c612ef5eda312d2338d4b56821ef754a4
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-28 18:03:00 +0530

    scroll to selected tab on Click functionality

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const[selectedTab,setSelectedTab]=useState("Home");
   return (
     <div className="app-container">
-    <Sidebar selectedTab={selectedTab}/>
+    <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
 
     <div className="content">
       <Header/>

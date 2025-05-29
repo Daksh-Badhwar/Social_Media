e39976742b9d5d36d85601d3c84ad532f266c932
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-29 15:31:05 +0530

    Delete Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,35 @@
+import { MdDelete } from "react-icons/md";
+import {PostList} from "../store/post-list-store";
+import {useContext} from "react";
+
 const Post=({post})=>{
+
+const{deletePost}= useContext(PostList)
+
+
+
+
  return(
-    <div class="card post-card" style={{width: "18rem"}}>
+    <div class="card post-card" style={{width: "30rem"}}>
   
   <div class="card-body">
     <h5 class="card-title">{post.title}
    
-  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    {post.reactions}
+  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" onClick={()=>deletePost(post.id)}>
+     <MdDelete />
   </span>
   </h5>
     <p class="card-text">{post.body}</p>
 
     {post.tags.map((tag)=>
-    <span class="badge text-bg-primary hash-tag">{tag}</span>
+    <span key={tag} class="badge text-bg-primary hash-tag">{tag}</span>
     )}
     
     
   </div>
+  <div class="alert alert-success reactions" role="alert">
+   This post has been reacted by {post.reactions} people.
+</div>
 </div>
  )
 }
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -8,7 +8,12 @@ export const PostList= React.createContext({
 });
 
 const postListReducer =(currPostList,action)=>{
-    return currPostList;
+    let newPostList=currPostList;
+    if (action.type==='DELETE_POST')
+    {
+      newPostList=currPostList.filter((post)=>post.id!==action.payload.postId)
+    }
+    return newPostList;
 }
 
 
@@ -19,7 +24,14 @@ const PostListProvider=({children})=>{
 
   const addPost=()=>{};
 
-  const deletePost=()=>{};
+  const deletePost=(postId)=>{
+    dispatchPostList({
+        type:"DELETE_POST",
+        payload:{
+            postId,
+        },
+    });
+  };
 
 
 

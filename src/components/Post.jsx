9e17570c6ffda4e567fdeaab004072fe0b94e8ff
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-29 13:29:15 +0530

    Adding Badges(Reactions)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,18 @@ const Post=({post})=>{
     <div class="card post-card" style={{width: "18rem"}}>
   
   <div class="card-body">
-    <h5 class="card-title">{post.title}</h5>
+    <h5 class="card-title">{post.title}
+   
+  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    {post.reactions}
+  </span>
+  </h5>
     <p class="card-text">{post.body}</p>
+
+    {post.tags.map((tag)=>
+    <span class="badge text-bg-primary hash-tag">{tag}</span>
+    )}
+    
     
   </div>
 </div>
